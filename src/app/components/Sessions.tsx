13d7a416dcb0e4d2dee33dbf92374648060b7158
AnthonyPetros <93829738+AnthonyPetros@users.noreply.github.com>
2024-11-17 20:01:41 -0500

    styled stuff a bit

--- src/app/components/Sessions.tsx
+++ src/app/components/Sessions.tsx
@@ -33,22 +33,27 @@ import { useState } from 'react';
 
      return (
        <div>
-         <h1 className="text-3xl font-bold underline">Poker Tracker App</h1>
+         <h1 className="text-3xl font-bold underline flex flex-row  justify-center">Poker Tracker App</h1>
          <input
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className=" my-2 bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
            type="text"
            value={newSessionResult}
            onChange={(e) => setNewSessionResult(e.target.value)}
          />
-         <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={addSession}>Add Session</button>
-         <ul>
+         <div className='flex flex-row  justify-center'>
+          <button className='my-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={addSession}>Add Session</button>
+        </div>
+         <ul className='my-4'>
            {sessions.map((session) => (
-             <li key={session.id}>
-
-               <span className={session.won ? 'text-black font-bold px-4 rounded' : 'text-red-500 font-bold px-4 rounded' }>
-                 {session.result}
-               </span>
-               <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => removeSession(session.id)}>Remove</button>
+             <li key={session.id} >
+                <div className='flex flex-row  justify-center'>
+                  <div className=' hover:ring-4 ring-offset-2 ring-2 w-1/3 rounded'>
+                    <span className={session.won ? 'text-green-500 font-bold px-4' : 'text-red-500 font-bold px-4' }>
+                      Result: {session.result}
+                    </span>
+                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => removeSession(session.id)}>Remove</button>
+                </div>
+               </div>
              </li>
            ))}
          </ul>
