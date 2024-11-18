d6784bd23238acf02bdb5d3caae5ee47d4dba49a
AnthonyPetros <93829738+AnthonyPetros@users.noreply.github.com>
2024-11-18 13:57:05 -0500

    added textarea and fully populated modal fields

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -9,6 +9,10 @@ import {
 } from "@/components/ui/card"
 
 import { DateTimePicker } from '@/components/ui/dateTimePicker';
+import { Textarea } from "@/components/ui/textarea"
+import { Label } from "@/components/ui/label"
+
+
 
 
 interface ModalProps {
@@ -19,8 +23,8 @@ interface ModalProps {
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose,  children}) => {
   if (!isOpen) return null;
-  const [date12, setDate12] = useState<Date | undefined>(undefined);
-  const [date24, setDate24] = useState<Date | undefined>(undefined);
+  const [date12Start, setDate12Start] = useState<Date | undefined>(undefined);
+  const [date12End, setDate12End] = useState<Date | undefined>(undefined);
 
   return (
    <Card>
@@ -34,11 +38,35 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose,  children}) => {
         </button>
       </div>
     </CardHeader>
-      <CardContent className='grid grid-cols-4'>
-      <div>
-        <span>Start Time:</span>
-        <DateTimePicker hourCycle={12} value={date12} onChange={setDate12} />
-      </div>
+      <CardContent className='grid grid-cols-2 gap-1.5'>
+        <div>
+          <Label htmlFor="buyIn">Buy In:</Label>
+          <Textarea id="buyIn" className='h-10'  placeholder="Enter how much you bought in for."></Textarea>
+        </div>
+        <div>
+          <Label htmlFor="cashOut">Cashout:</Label>
+          <Textarea id="cashOut" className='h-10' placeholder="Enter how much you cashed out for."></Textarea>
+        </div>
+        <div>
+          <span>Start Time:</span>
+          <DateTimePicker hourCycle={12} value={date12Start} onChange={setDate12Start} />
+        </div>
+        <div>
+          <span>End Time:</span>
+          <DateTimePicker hourCycle={12} value={date12End} onChange={setDate12End} />
+        </div>
+        <div>
+          <Label htmlFor="stakes">Stakes:</Label>
+          <Textarea id="stakes" className='h-10' placeholder="What Stakes were played"></Textarea>
+        </div>
+        <div>
+          <Label htmlFor="game">Game Type:</Label>
+          <Textarea id="game" className='h-10' placeholder="What Game was played"></Textarea>
+        </div>
+        <div>
+          <Label htmlFor="location">Location:</Label>
+          <Textarea id="location" className='h-10' placeholder="Where was this played"></Textarea>
+        </div>
       
       </CardContent>
    </Card>
