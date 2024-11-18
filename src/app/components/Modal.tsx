e33c3f95bc222800f70eefcbf59cd09df1f9905e
AnthonyPetros <93829738+AnthonyPetros@users.noreply.github.com>
2024-11-18 12:07:08 -0500

    added datetime pickers and styled modal a bit more

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,4 +1,15 @@
 import React, { useState } from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import { DateTimePicker } from '@/components/ui/dateTimePicker';
+
 
 interface ModalProps {
   isOpen: boolean;
@@ -8,20 +19,30 @@ interface ModalProps {
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose,  children}) => {
   if (!isOpen) return null;
+  const [date12, setDate12] = useState<Date | undefined>(undefined);
+  const [date24, setDate24] = useState<Date | undefined>(undefined);
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-md shadow-lg">
-        <div className=" top-0 right-0 h-16 w-16">
-          <button onClick={onClose} >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
-        {children}
+   <Card>
+    <CardHeader className='grid grid-cols-2 bg-blue-300'>     
+      <CardTitle className='py-2'>Add a new session</CardTitle>
+      <div className="justify-self-end">
+        <button onClick={onClose} >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="size-6">
+            <path d="M6 18 18 6M6 6l12 12" />
+          </svg>
+        </button>
       </div>
-    </div>
+    </CardHeader>
+      <CardContent className='grid grid-cols-4'>
+      <div>
+        <span>Start Time:</span>
+        <DateTimePicker hourCycle={12} value={date12} onChange={setDate12} />
+      </div>
+      
+      </CardContent>
+   </Card>
+      
   );
 };
 
--- src/app/components/Sessions.tsx
+++ src/app/components/Sessions.tsx
@@ -3,6 +3,15 @@ import Modal from './Modal';
 
 import { Button } from "@/components/ui/button"
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
    interface SessionItem {
         id: string;
         result: number;
@@ -45,38 +54,33 @@ import { Button } from "@/components/ui/button"
 
      return (
        <div>
-         <h1 className="text-3xl font-bold underline flex flex-row  justify-center">Poker Bankroll Tracker</h1>
-         <input
-          className=" my-2 bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-           type="text"
-           value={newSessionResult}
-           onChange={(e) => setNewSessionResult(e.target.value)}
-         />
-         <div>
-         <Button>Click me</Button>
-        </div>
-         <div className='flex flex-row  justify-center'>
-          <button className='my-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={handleOpenModal}>Add Session</button>
-        </div>
-        
-         <ul className='my-4'>
-           {sessions.map((session) => (
-             <li className='my-3' key={session.id} >
-                <div className='flex flex-row  justify-center'>
-                  <div className=' hover:ring-4 ring-offset-2 ring-2 w-1/3 rounded'>
-                    <span className={session.won ? 'text-green-500 font-bold px-4' : 'text-red-500 font-bold px-4' }>
-                      Result: {session.result}
-                    </span>
-                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => removeSession(session.id)}>Remove</button>
+        <div className={isCreateSessionModalOpen ? 'blur':''}>
+          <h1 className="text-3xl font-bold underline flex flex-row  justify-center">Poker Bankroll Tracker</h1>
+          
+          <div className='flex flex-row  justify-center'>
+            <button className='my-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={handleOpenModal}>Add Session</button>
+          </div>
+          
+          <ul className='my-4'>
+            {sessions.map((session) => (
+              <li className='my-3' key={session.id} >
+                  <div className='flex flex-row  justify-center'>
+                    <div className=' hover:ring-4 ring-offset-2 ring-2 w-1/3 rounded'>
+                      <span className={session.won ? 'text-green-500 font-bold px-4' : 'text-red-500 font-bold px-4' }>
+                        Result: {session.result}
+                      </span>
+                      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => removeSession(session.id)}>Remove</button>
+                  </div>
                 </div>
-               </div>
-             </li>
-           ))}
-         </ul>
-         <Modal isOpen={isCreateSessionModalOpen} onClose={handleCloseModal}>
-            <h1>testtesttesttesttesttesttesttesttesttesttest</h1>
-            <p>test</p>
-          </Modal>
+              </li>
+            ))}
+          </ul>
+         </div>
+         <div className='w-2/3 justify-self-center'>
+          <Modal  isOpen={isCreateSessionModalOpen}  onClose={handleCloseModal}>
+            <></>
+            </Modal>
+          </div>
        </div>
      );
    };
