55d33fbc086b5cf27d79def7566a7b9e1ee647bf
AnthonyPetros <93829738+AnthonyPetros@users.noreply.github.com>
2024-11-21 15:49:11 -0500

    added deleting and editing sessions

--- src/lib/sessionData.ts
+++ src/lib/sessionData.ts
@@ -21,11 +21,19 @@ export async function getAllSessionData(){
 }
 
 export async function getSessionDataById(id: string){
-   const h = id;
-   console.log(h);
     const sql = neon(process.env.DATABASE_URL!);
-    let res = await sql`SELECT * from poker_sessions where id = ${h}`; 
-    console.log(res);
+    let res = await sql`SELECT * from poker_sessions where id = ${id}`; 
     return JSON.stringify(res);
+}
+
+export async function editSessionData(session: SessionItem){
+    const sql = neon(process.env.DATABASE_URL!);
+    await sql`update poker_sessions set buy = ${session.buy}, cash = ${session.cash},
+    stime = ${session.stime}, etime = ${session.etime}, stakes = ${session.stakes}, location = ${session.location} where id = ${session.id}`; 
+}
+
+export async function deleteSessionDataById(id: string){
+    const sql = neon(process.env.DATABASE_URL!);
+    await sql`delete from poker_sessions where id = ${id}`;
 }
 
